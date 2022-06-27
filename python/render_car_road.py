ca389651139b466f89c2103fb8eb3c15112b2b7d
Grace Su <grace.duansu@gmail.com>
2022-06-27 19:19:28 -0400

    finished quotient_compose; does not preserve alpha

--- python/render_car_road.py
+++ python/render_car_road.py
@@ -4,11 +4,13 @@
 import os
 from overlay import overlay
 from alpha_blend import alpha_blend
+from quotient_compose import quotient_compose
 # import xml.etree.ElementTree as ET
 from lxml import etree as ET
+import cv2
+import numpy as np
 
-
-def generate_xml(xml_file, cam_to_world_matrix, render_ground, cars_list):
+def generate_xml(xml_file, cam_to_world_matrix, cars_list, render_ground=True, render_cars=True):
     
     tree = ET.parse('../assets/car_road_template.xml')
     root = tree.getroot()
@@ -17,31 +19,40 @@ def generate_xml(xml_file, cam_to_world_matrix, render_ground, cars_list):
     sensor_matrix.set('value', cam_to_world_matrix) 
 
 
-    if render_ground:
-        ground_string = '''<shape type="obj"> 
-            <string name="filename" value="assets/ground.obj" /> 
-            <bsdf type="diffuse"> 
-                <spectrum name="reflectance" value="0.1"/> 
-            </bsdf> 
-        </shape>'''
-
-        ground = ET.fromstring(ground_string)
-        root.append(ground)
-
     for car in cars_list:   
-        car_string = '''<shape type="obj">
-            <string name="filename" value="{}"/>
-            <transform name="toWorld">
-                <scale value="{}"/>
+        if render_cars:
+            car_string = '''<shape type="obj">
+                <string name="filename" value="{}"/>
+                <transform name="toWorld">
+                    <scale value="{}"/>
 
-                <rotate x="0" y="1" z="0" angle="{}" />
-                <translate x="{}" y="{}" z="{}" />
-            </transform>
-        </shape>
-        '''.format(car['obj'], car['scale'], car['y_rotate'], car['x'], car['y'], car['z'])
+                    <rotate x="0" y="1" z="0" angle="{}" />
+                    <translate x="{}" y="{}" z="{}" />
+                </transform>
+            </shape>
+            '''.format(car['obj'], car['scale'], car['y_rotate'], car['x'], car['y'], car['z'])
 
-        car_element = ET.fromstring(car_string)
-        root.append(car_element)
+            car_element = ET.fromstring(car_string)
+            root.append(car_element)
+
+        if render_ground:
+            ground_string = '''<shape type="obj">
+        <string name="filename" value="assets/ground.obj" />
+        <transform name="toWorld">
+            <scale value="0.03" />
+            <rotate x="0" y="1" z="0" angle="{}" />
+            <translate x="{}" y="{}" z="{}" />
+        </transform>
+
+        <bsdf type="roughdiffuse">
+            <spectrum name="reflectance" value="0.5" />
+            <float name="alpha" value="0.2" />
+        </bsdf>
+
+    </shape>'''.format(car['y_rotate'], car['x'], 0, car['z'])
+
+            ground = ET.fromstring(ground_string)
+            root.append(ground)
 
 
     tree.write(xml_file, encoding='utf-8', xml_declaration=True)
@@ -67,8 +78,8 @@ MITSUBA_ARGS = {'turbidity':3, 'latitude':40.44694, 'longitude':-79.94902,
     'fov':81, 'sampleCount':16, 'width':1000, 'height':750}
 
 
-def main(output_dir, xml_file, cam_to_world_matrix, render_ground, cars_list, 
-        bg_img_path, rendered_img_name, composite_img_name, use_alpha_blend, **kwargs):
+def main(output_dir, xml_name, cam_to_world_matrix, cars_list, 
+        bg_img_path, rendered_img_name, composite_img_name, compose_mode, **kwargs):
     """
     See MITSUBA_ARGS dict initialization above for optional kwargs
     """
@@ -79,62 +90,95 @@ def main(output_dir, xml_file, cam_to_world_matrix, render_ground, cars_list,
         cars_list[i]['z'] = calculate_car_pos(cars_list[i]['line_slope'], 
             cars_list[i]['line_displacement'], cars_list[i]['x'])
     
-    xml_path = output_dir + xml_file
-    generate_xml(xml_path, cam_to_world_matrix, render_ground, cars_list)
+    # Im_all
+    xml_path = output_dir + xml_name + ".xml"
+    generate_xml(xml_path, cam_to_world_matrix, cars_list, render_cars=True, render_ground=True)
+
+    if compose_mode == "quotient":
+        # Im_pl
+        xml_path_pl = output_dir + xml_name + "_pl.xml"
+        generate_xml(xml_path_pl, cam_to_world_matrix, cars_list, render_cars=False, render_ground=True)
+
+        # Im_obj
+        xml_path_obj = output_dir + xml_name + "_obj.xml"
+        generate_xml(xml_path_obj, cam_to_world_matrix, cars_list, render_cars=True, render_ground=False)
 
     # handle kwargs
     for key in kwargs:
         MITSUBA_ARGS[key] = kwargs[key]
 
-    cli_args = " -o " + rendered_img_name
+    cli_args = ""
     for key in MITSUBA_ARGS:
         cli_args += " -D {}={} ".format(key, MITSUBA_ARGS[key])
 
     # generate mitsuba command
-    print("Run the following command to render:")
+    print("Run the following commands to render:")
     print()
-    print("mitsuba" + cli_args + " " + xml_file)
+    print("mitsuba" + cli_args + " -o " + rendered_img_name + " " + xml_name + ".xml")
     print()
+
+    pl_img = xml_name + "_pl.png"
+    obj_img = xml_name + "_obj.png"
+    if compose_mode == "quotient":
+        print("mitsuba" + cli_args + " -o " + pl_img + " " + xml_name + "_pl.xml")
+        print()
+        print("mitsuba" + cli_args + " -o " + obj_img + " " + xml_name + "_obj.xml")
+        print()
 
     done = input('Hit enter after rendering is complete to generate composite:\n')
 
     rendered_img_path = output_dir + rendered_img_name
     composite_img_path = output_dir + composite_img_name
 
-    # overlay or alpha_blend onto bg_img_path
-    if use_alpha_blend:
+    # compose render onto bg_img_path
+    if compose_mode == "alpha":
         alpha_blend(bg_img_path, rendered_img_path, composite_img_path)
         print('Alpha blending for {} complete'.format(composite_img_path))
-    else:
+    elif compose_mode == "overlay":
         overlay(bg_img_path, rendered_img_path, composite_img_path)
+        print('Overlay for {} complete'.format(composite_img_path))
+    elif compose_mode == "quotient":
+        quotient_compose(bg_img_path, rendered_img_path, composite_img_path, 
+            output_dir + pl_img, output_dir + obj_img)
         print('Overlay for {} complete'.format(composite_img_path))
    
     
 
 if __name__ == '__main__':
+    # bg_img_path = "/home/gdsu/scenes/city_test/assets/cam2_week1_forward_2021-05-01T14-43-40.623202.jpg"
+    # rendered_img_path = "/home/gdsu/scenes/city_test/invRendTest.png"
+    # composite_img_path = "invRendTest_composite.png"
+    # alpha_blend(bg_img_path, rendered_img_path, composite_img_path)
+
     ######### Required arguments. Modify as desired: #############
+    
     output_dir = "/home/gdsu/scenes/city_test/"
-    xml_file = "generated.xml"
+    xml_name = "quotientTest"
     cam_to_world_matrix = '-6.32009074e-01 3.81421015e-01  6.74598057e-01 -1.95597297e+01 '\
         '5.25615099e-03 8.72582680e-01 -4.88438164e-01  6.43714192e+00 '\
         '-7.74943161e-01  -3.05151563e-01 -5.53484978e-01  4.94516235e+00 '\
         '0 0 0 1'
-    render_ground = True
 
-    # car z position can be calculated later according to line equation
+    # car z position will be calculated later according to line equation
     cars_list = [
-        {"obj": "../cars_test/meshes/car/857a3a01bd311511f200a72c9245aee7/model.obj", 
-        "x": -17, "y": 0.16, "z": None, "scale": 5, "y_rotate": 225, 
-        "line_slope":-0.95, "line_displacement":-16.19},
+        {"obj": "../cars_test/meshes/car/1e2f9cb6b33c92ea82381b04bbe0ce6d/model.obj", 
+        "x": -2, "y": 0.8, "z": None, "scale": 5, "y_rotate": 315, 
+        "line_slope":0.87, "line_displacement":3},
 
-        {"obj": "../cars_test/meshes/car/ff5ad56515bc0167500fb89d8b5ec70a/model.obj", 
-        "x": 4, "y": 0.16, "z": None, "scale": 5, "y_rotate": 225,
-        "line_slope":-0.95, "line_displacement":-16.19}
+        # {"obj": "../cars_test/meshes/car/ff5ad56515bc0167500fb89d8b5ec70a/model.obj", 
+        # "x": -6, "y": 0.8, "z": None, "scale": 5, "y_rotate": 225,
+        # "line_slope":-0.95, "line_displacement":-16.19}
         ]
-    bg_img_path = "../assets/cam2_week1_forward_2021-05-01T14-43-40.623202.jpg"
-    rendered_img_name = "generated.png"
-    composite_img_name = "generated_composite.png"
-    use_alpha_blend = True # if False, use overlay instead
 
-    main(output_dir, xml_file, cam_to_world_matrix, render_ground, cars_list, 
-        bg_img_path, rendered_img_name, composite_img_name, use_alpha_blend, turbidity=5)
+    bg_img_path = "../assets/cam2_week1_forward_2021-05-01T14-43-40.623202.jpg"
+    compose_mode = "quotient" # "alpha", "overlay", or "quotient"
+
+
+    rendered_img_name = xml_name + ".png"
+    composite_img_name = xml_name + "_" + compose_mode + "_composite.png"
+    
+
+    main(output_dir, xml_name, cam_to_world_matrix, cars_list, 
+        bg_img_path, rendered_img_name, composite_img_name, compose_mode, 
+        width=2000,height=1500)
+    
